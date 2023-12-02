17e990d54737ac6bc022278c74595363a1e04978
akiraly <kiralyattila.hu@gmail.com>
2023-12-02 21:51:47 +0100

    Day2 part 1

--- src/main/kotlin/Day02p1.kt
+++ src/main/kotlin/Day02p1.kt
@@ -1,1 +1,41 @@
 package day02p1
+
+fun List<Game>.idSumOfPossibleGames(bag: Cubes): Int = filter { it.isPossible(bag) }.sumOf { it.id }
+
+data class Game(val id: Int, val handfuls: List<Cubes>) {
+    fun isPossible(bag: Cubes): Boolean = handfuls.all { bag.canContain(it) }
+
+    companion object {
+        fun from(raw: String): Game {
+            val (gameText, handfulTexts) = raw.split(": ")
+            val id = gameText.split(" ")[1].toInt()
+            val handfuls = handfulTexts.split("; ").map { Cubes.from(it) }
+            return Game(id, handfuls)
+        }
+
+        fun gameListFrom(raw: String): List<Game> = raw.lines().map { from(it) }
+    }
+}
+
+data class Cubes(val red: Int = 0, val green: Int = 0, val blue: Int = 0) {
+    fun canContain(other: Cubes): Boolean =
+        red >= other.red && green >= other.green && blue >= other.blue
+
+    companion object {
+        private val reds = """(\d+) red""".toRegex()
+
+        private val greens = """(\d+) green""".toRegex()
+
+        private val blues = """(\d+) blue""".toRegex()
+
+        private fun Regex.parseNumOfCubes(raw: String) = find(raw)?.let {
+            it.groupValues[1].toInt()
+        } ?: 0
+
+        fun from(raw: String): Cubes = Cubes(
+            red = reds.parseNumOfCubes(raw),
+            green = greens.parseNumOfCubes(raw),
+            blue = blues.parseNumOfCubes(raw)
+        )
+    }
+}
--- src/test/kotlin/Day02p1Tests.kt
+++ src/test/kotlin/Day02p1Tests.kt
@@ -2,6 +2,7 @@ package day02p1
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.readInput
 
 class Day02p1Tests : BehaviorSpec({
     Given("a number of red, green and blue cubes") {
@@ -40,6 +41,13 @@ class Day02p1Tests : BehaviorSpec({
                 canContain shouldBe true
             }
         }
+
+        When("we test if the handful can contain the bag") {
+            val canContain = handful.canContain(bag)
+            Then("it should return false") {
+                canContain shouldBe false
+            }
+        }
     }
 
     Given("a game with an id and a series of handful of cubes") {
@@ -76,8 +84,8 @@ class Day02p1Tests : BehaviorSpec({
                 14,
                 listOf(
                     Cubes(green = 1, red = 3, blue = 6),
-                    Cubes(red = 1, green = 2, blue = 6),
-                    Cubes(green = 2)
+                    Cubes(green = 3, red = 6),
+                    Cubes(green = 3, blue = 15, red = 14)
                 )
             )
         ).forEach { (text, expectedGame) ->
@@ -91,38 +99,58 @@ class Day02p1Tests : BehaviorSpec({
             }
         }
     }
-})
 
-data class Game(val id: Int, val handfuls: List<Cubes>) {
-    companion object {
-        fun from(raw: String): Game {
-            val (gameText, handfulTexts) = raw.split(": ")
-            val id = gameText.split(" ")[1].toInt()
-            val handfuls = handfulTexts.split("; ").map { Cubes.from(it) }
-            return Game(id, handfuls)
+    Given("A game and a bag of cubes") {
+        val bag = Cubes(red = 12, green = 13, blue = 14)
+        And("the bag is $bag") {
+            listOf(
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" to true,
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" to true,
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" to false,
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red" to false,
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" to true
+            ).forEach { (gameText, expectedPossibility) ->
+                And("the game is $gameText") {
+                    val game = Game.from(gameText)
+                    When("we test if the game is possible") {
+                        val possible = game.isPossible(bag)
+                        Then("it should return $expectedPossibility") {
+                            possible shouldBe expectedPossibility
+                        }
+                    }
+                }
+            }
         }
     }
-}
 
-data class Cubes(val red: Int = 0, val green: Int = 0, val blue: Int = 0) {
-    fun canContain(other: Cubes): Boolean =
-        red >= other.red && green >= other.green && blue >= other.blue
-
-    companion object {
-        private val reds = """(\d+) red""".toRegex()
-
-        private val greens = """(\d+) green""".toRegex()
-
-        private val blues = """(\d+) blue""".toRegex()
-
-        private fun Regex.parseNumOfCubes(raw: String) = find(raw)?.let {
-            it.groupValues[1].toInt()
-        } ?: 0
-
-        fun from(raw: String): Cubes = Cubes(
-            red = reds.parseNumOfCubes(raw),
-            green = greens.parseNumOfCubes(raw),
-            blue = blues.parseNumOfCubes(raw)
+    Given("An example list of games and a bag of cubes") {
+        val games = Game.gameListFrom(
+            """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """.trimIndent()
         )
+        val bag = Cubes(red = 12, green = 13, blue = 14)
+        When("we sum the ids of the games that are possible") {
+            val sum = games.idSumOfPossibleGames(bag)
+            Then("we should get the correct sum") {
+                sum shouldBe 8
+            }
+        }
     }
-}
+
+    Given("Our custom input list of games and a bag of cubes") {
+        val games = Game.gameListFrom(readInput("day02p1"))
+        val bag = Cubes(red = 12, green = 13, blue = 14)
+        When("we sum the ids of the games that are possible") {
+            val sum = games.idSumOfPossibleGames(bag)
+            Then("we should get the correct sum") {
+                sum shouldBe 2505
+            }
+        }
+    }
+
+})
