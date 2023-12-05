787d699749df3c62dc81d1e2b462e4a600b994b5
akiraly <kiralyattila.hu@gmail.com>
2023-12-05 16:32:04 +0100

    Adding an .editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,34 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+  kotlin("jvm") version "1.9.21"
 }
 
 kotlin {
-    jvmToolchain(21)
+  jvmToolchain(21)
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("commons-io:commons-io:2.15.1")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation("com.google.guava:guava:32.1.3-jre")
+  implementation("org.apache.commons:commons-collections4:4.4")
+  implementation("commons-io:commons-io:2.15.1")
+  implementation("org.apache.commons:commons-lang3:3.14.0")
+  implementation("org.apache.commons:commons-math3:3.6.1")
+  implementation("com.google.guava:guava:32.1.3-jre")
 
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-    testImplementation("io.kotest:kotest-framework-datatest:5.8.0")
+  testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+  testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+  testImplementation("io.kotest:kotest-framework-datatest:5.8.0")
 }
 
 tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,11 @@
 ## Morgan Stanley Advent of TDD (Advent of Code 2023 solutions in TDD style)
 
 * Kotlin 1.9.21
+* [Kotest](https://kotest.io/) for testing
 * JVM 21
-* Kotest for testing
+* Gradle 8.5
+* Intellij 2023.2.5 with latest stable Kotlin and Kotest plugins
 
-Kotest has a rich set of features including multiple different testing styles.
+Kotest has a rich set of features including multiple
+different [testing styles](https://kotest.io/docs/framework/testing-styles.html).
 Different days sometimes use different styles because I am experimenting with them.
