2edf1432762d9560729b0499549c421fd4ad00c3
akiraly <kiralyattila.hu@gmail.com>
2023-12-15 00:27:12 +0100

    Day 13 part 1

---
+++ src/main/kotlin/Dday13p1.kt
@@ -0,0 +1,60 @@
+package day13p1
+
+import kotlin.math.min
+
+fun List<Pattern>.sumOfScoredMirrorPositions(): Long = sumOf { it.findScoredMirrorPosition() }
+
+fun String.parsePatterns(): List<Pattern> = split("\n\n").map { it.toPattern() }
+
+fun String.toPattern(): Pattern = Pattern(lines().map { it.toList() })
+
+data class Pattern(val lines: List<List<Char>>) {
+
+  fun findHorizontalMirrorPosition(): Int = findHorizontalMirrorPositions().firstOrNull() ?: -1
+
+  fun findVerticalMirrorPosition(): Int = transpose().findHorizontalMirrorPosition()
+
+  fun findHorizontalMirrorPositions(): Set<Int> {
+    val length = lines.first().size
+    var candidates = (1..<length).map { it to min(it, length - it) }.toList()
+
+    for (line in lines) {
+      candidates = candidates.filter { (position, mirrorSize) ->
+
+        (position - mirrorSize..<position).zip(position + mirrorSize - 1 downTo position)
+          .all { (a, b) -> line[a] == line[b] }
+      }
+
+      if (candidates.isEmpty()) return emptySet()
+    }
+
+    return candidates.map { it.first }.toSet()
+  }
+
+  private fun transpose(): Pattern = Pattern(lines.transpose())
+
+  fun toPatternString(): String = lines.joinToString("\n") { it.joinToString("") }
+  fun findScoredMirrorPosition(): Long {
+    var position = findHorizontalMirrorPosition()
+    if (position != -1) return position.toLong()
+
+    position = findVerticalMirrorPosition()
+
+    //if (position == -1) return 0
+    require(position > 0) { "No mirror position found: ${lines.joinToString("\n") { it.joinToString("") }}" }
+
+    return position * 100L
+  }
+}
+
+fun List<String>.transposeStrings(): List<String> = asSequence()
+  .map { it.toList() }
+  .transpose()
+  .map { it.joinToString("") }
+  .toList()
+
+fun List<List<Char>>.transpose(): List<List<Char>> = asSequence().transpose().toList()
+
+fun Sequence<List<Char>>.transpose(): Sequence<List<Char>> =
+  (0 until first().size).asSequence().map { col -> map { it[col] }.toList() }
+
---
+++ src/test/kotlin/Day13p1Tests.kt
@@ -0,0 +1,145 @@
+package day13p1
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.shouldBe
+import io.readInput
+
+class Day13p1Tests : FunSpec({
+  val examplePattern1 = """
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+  """.trimIndent()
+
+  val examplePattern2 = """
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+  """.trimIndent()
+
+  val examplePatterns = """
+$examplePattern1
+
+$examplePattern2
+  """.trimIndent()
+
+  test("Given the example patterns then it can be parsed correctly as 2 patterns") {
+    val patterns = examplePatterns.parsePatterns()
+    patterns.size shouldBe 2
+    patterns[0].lines.size shouldBe 7
+    patterns[0].toPatternString() shouldBe examplePattern1
+    patterns[1].toPatternString() shouldBe examplePattern2
+  }
+
+  context("test transpose") {
+    test(""" Given listOf("ab") then transposed it is expected to be listOf("a", "b") """) {
+      listOf("ab").transposeStrings() shouldBe listOf("a", "b")
+    }
+
+    table(
+      headers("Pattern", "Transposed"),
+      row(listOf(listOf('a')), listOf(listOf('a'))),
+      row(listOf(listOf('a', 'b')), listOf(listOf('a'), listOf('b'))),
+      row(listOf(listOf('a', 'b', 'c')), listOf(listOf('a'), listOf('b'), listOf('c'))),
+    ).forAll { pattern, expected ->
+      test(""" Given $pattern then transposed it is expected to be $expected """) {
+        pattern.transpose() shouldBe expected
+      }
+    }
+  }
+
+  context("Finding mirror position in a pattern") {
+    table(
+      headers("Pattern", "Position"),
+      rows = examplePattern1.lines().map { row -> row(row, 5) }
+    ).forAll { row, expectedPosition ->
+      test(""" Given a pattern of "$row" then the expected horizontal mirror position is $expectedPosition """) {
+        val pattern = row.toPattern()
+        pattern.findHorizontalMirrorPositions() shouldContain expectedPosition
+      }
+    }
+
+    test(""" Given example pattern 1 then the expected (horizontal) mirror position is 5 """) {
+      val pattern = examplePattern1.toPattern()
+      pattern.findHorizontalMirrorPosition() shouldBe 5
+      pattern.findVerticalMirrorPosition() shouldBe -1
+      pattern.findScoredMirrorPosition() shouldBe 5
+    }
+
+    test(""" Given example pattern 2 then the expected (vertical) mirror position is 4 """) {
+      val pattern = examplePattern2.toPattern()
+      pattern.findVerticalMirrorPosition() shouldBe 4
+      pattern.findHorizontalMirrorPosition() shouldBe -1
+      pattern.findScoredMirrorPosition() shouldBe 400
+    }
+
+    test(""" Given example pattern 3 then the expected (horizontal) mirror position is 16""") {
+      val pattern = """
+        ..#...####...#...
+        ....#.#..#.#.....
+        ..##.######.##...
+        ...#.######.#....
+        #.###..##...##.##
+        ##..##.##.##..###
+        ......####.......
+        ...###.##.###....
+        ....###..###.....
+        .##.#......#.##..
+        .#.#........#.#..
+        .##...#..#...##..
+        #..#.#....#.#..##
+      """.trimIndent().toPattern()
+
+      pattern.findHorizontalMirrorPosition() shouldBe 16
+      pattern.findVerticalMirrorPosition() shouldBe -1
+      pattern.findScoredMirrorPosition() shouldBe 16
+    }
+
+    test(""" Given example pattern 3 then the expected (horizontal) mirror position is """) {
+      val pattern = """
+        ..#...####...#...
+        ....#.#..#.#.....
+        ..##.######.##...
+        ...#.######.#....
+        #.##...##...##.##
+        ##..##.##.##..###
+        ......####.......
+        ...###.##.###....
+        ....###..###.....
+        .##.#......#.##..
+        .#.#........#.#..
+        .##...#..#...##..
+        #..#.#....#.#..##
+      """.trimIndent().toPattern()
+
+      pattern.findHorizontalMirrorPosition() shouldBe 8
+      pattern.findVerticalMirrorPosition() shouldBe -1
+      pattern.findScoredMirrorPosition() shouldBe 8
+    }
+
+    table(
+      headers("Title", "Patterns", "Number of patterns parsed", "Expected Score"),
+      row("Example Patterns", examplePatterns, 2, 405),
+      row("Custom Patterns", readInput("day13p1"), 100, 33780),
+    ).forAll { title, patterns, numberOfParts, score ->
+      test(""" Given $title then the expected score is $score """) {
+        val parsedPatterns = patterns.parsePatterns()
+        parsedPatterns.size shouldBe numberOfParts
+        parsedPatterns.sumOfScoredMirrorPositions() shouldBe score
+      }
+    }
+  }
+})
