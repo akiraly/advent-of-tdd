7883050c3d9ad2768f24e94fca87e8cbf4c84cae
akiraly <kiralyattila.hu@gmail.com>
2023-12-24 03:44:41 +0100

    Day 22 part 2

---
+++ src/main/kotlin/Day22p2.kt
@@ -0,0 +1,136 @@
+package day22p2
+
+fun String.sumOfOtherBricksToFallInChainReactions(): Int =
+  toSnapshot().settle().sumOfOtherBricksToFallInChainReactions()
+
+fun String.numberOfBricksThatCanBeDisintegrated(): Int = toSnapshot().settle().numberOfBricksThatCanBeDisintegrated()
+
+fun String.toSnapshot(): Snapshot = Snapshot(
+  this
+    .lineSequence()
+    .map { line -> line.split("~").map { it.split(",").map { n -> n.toInt() } } }
+    .mapIndexed { index, parts ->
+      Brick(index + 1, parts[0][0], parts[0][1], parts[0][2], parts[1][0], parts[1][1], parts[1][2])
+    }
+    .associateBy { it.id }
+)
+
+data class Snapshot(val bricks: Map<BrickId, Brick>) {
+  constructor(vararg bricks: Brick) : this(bricks.associateBy { it.id })
+
+  val maxX: Int = bricks.values.maxOf { it.end.x }
+  val maxY: Int = bricks.values.maxOf { it.end.y }
+  val maxZ: Int = bricks.values.maxOf { it.end.z }
+
+  fun settle(): Snapshot {
+
+    val bricksByMinZ = bricks.values.asSequence().sortedBy { it.start.z }.toMutableList()
+
+    val topDownXYView: MutableMap<Coordinate2D, Pair<Int, Brick>?> =
+      (0..maxX).flatMap { x -> (0..maxY).asSequence().map { y -> Coordinate2D(x, y) to null } }
+        .toMap(mutableMapOf())
+
+    val settledBricks = mutableListOf<Brick>()
+
+    while (bricksByMinZ.isNotEmpty()) {
+      val current = bricksByMinZ.removeFirst()
+      val supporterCandidates = current.xyCoordinates().mapNotNull { topDownXYView[it] }
+
+      val (supporterZ, supporters) = supporterCandidates.maxOfOrNull { it.first }?.let { max ->
+        max to supporterCandidates.asSequence().filter { it.first == max }.map { it.second.id }.toSet()
+      } ?: (0 to emptySet())
+
+      val updated = current.copy(
+        start = current.start.copy(z = supporterZ + 1),
+        end = current.end.copy(z = supporterZ + 1 + current.end.z - current.start.z),
+        supporters = supporters
+      )
+
+      settledBricks.add(updated)
+
+      updated.xyCoordinates().forEach { xy -> topDownXYView[xy] = updated.end.z to updated }
+    }
+
+    return Snapshot(settledBricks.associateBy { it.id })
+  }
+
+  fun bricksThatCanBeDisintegrated(): Set<BrickId> {
+    val result = bricks.keys.toMutableSet()
+    bricks.values.forEach { brick ->
+      if (brick.supporters.size == 1) {
+        result.remove(brick.supporters.single())
+      }
+    }
+    return result
+  }
+
+  fun numberOfBricksThatCanBeDisintegrated(): Int = bricksThatCanBeDisintegrated().size
+  fun sumOfOtherBricksToFallInChainReactions(): Int {
+    val chainReactionStarters =
+      bricks.values.asSequence().filter { it.supporters.size == 1 }.map { it.supporters.single() }.toSet()
+
+    return chainReactionStarters.sumOf { numOfOtherBricksToFallInChainReaction(it) }
+  }
+
+  private fun numOfOtherBricksToFallInChainReaction(id: BrickId): Int {
+    val supporterToSupportedsMap: Map<BrickId, Set<BrickId>> = bricks.values
+      .flatMap { supported -> supported.supporters.map { it to supported.id } }
+      .groupingBy { it.first }
+      .fold(emptySet()) { set, pair -> set + pair.second }
+
+    val supportedToSupportersMap: Map<BrickId, MutableSet<BrickId>> = supporterToSupportedsMap.asSequence()
+      .flatMap { (supporter, supporteds) -> supporteds.map { it to supporter } }
+      .groupingBy { it.first }
+      .fold(emptySet<BrickId>()) { set, pair -> set + pair.second }
+      .mapValues { it.value.toMutableSet() }
+
+    val disintegratedBricks = mutableListOf(id)
+    var disintegratedCount = 0
+    while (disintegratedBricks.isNotEmpty()) {
+      val current = disintegratedBricks.removeFirst()
+      supporterToSupportedsMap[current]?.forEach { supported ->
+        val supporters = supportedToSupportersMap.getValue(supported)
+        supporters.remove(current)
+        if (supporters.isEmpty()) {
+          disintegratedBricks.add(supported)
+          disintegratedCount++
+        }
+      }
+    }
+    return disintegratedCount
+  }
+}
+
+data class BrickId(val id: Int)
+
+data class Brick(
+  val id: BrickId,
+  val start: Coordinate3D,
+  val end: Coordinate3D,
+  val supporters: Set<BrickId> = emptySet()
+) {
+
+  constructor(id: Int, sx: Int, sy: Int, sz: Int, ex: Int, ey: Int, ez: Int, vararg supporters: Int) : this(
+    BrickId(id),
+    Coordinate3D(sx, sy, sz),
+    Coordinate3D(ex, ey, ez),
+    supporters.asSequence().map { BrickId(it) }.toSet()
+  )
+
+  init {
+    require(start.x <= end.x)
+    require(start.y <= end.y)
+    require(start.z <= end.z)
+    require(0 <= start.x)
+    require(0 <= start.y)
+    require(1 <= start.z)
+  }
+
+  fun xyCoordinates(): Set<Coordinate2D> =
+    (start.x..end.x).flatMap { x -> (start.y..end.y).map { y -> Coordinate2D(x, y) } }.toSet()
+}
+
+data class Coordinate2D(val x: Int, val y: Int)
+
+data class Coordinate3D(val x: Int, val y: Int, val z: Int)
+
---
+++ src/test/kotlin/Day22p2Tests.kt
@@ -0,0 +1,94 @@
+package day22p2
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.readInput
+
+class Day22p2Tests : FunSpec({
+  val exampleSnapshotInput = """
+1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9
+  """.trimIndent()
+
+  val customSnapshotInput = readInput("day22p1")
+
+  context("parsing") {
+    test(""" Given the example snapshot input then it can be parsed as expected""") {
+      val snapshot = exampleSnapshotInput.toSnapshot()
+
+      snapshot shouldBe Snapshot(
+        Brick(1, 1, 0, 1, 1, 2, 1),
+        Brick(2, 0, 0, 2, 2, 0, 2),
+        Brick(3, 0, 2, 3, 2, 2, 3),
+        Brick(4, 0, 0, 4, 0, 2, 4),
+        Brick(5, 2, 0, 5, 2, 2, 5),
+        Brick(6, 0, 1, 6, 2, 1, 6),
+        Brick(7, 1, 1, 8, 1, 1, 9),
+      )
+
+      snapshot.bricks.size shouldBe 7
+      snapshot.maxX shouldBe 2
+      snapshot.maxY shouldBe 2
+      snapshot.maxZ shouldBe 9
+    }
+
+    test(""" Given the custom snapshot input then it can be parsed as expected""") {
+      val snapshot = customSnapshotInput.toSnapshot()
+
+      snapshot.bricks.size shouldBe 1499
+      snapshot.maxX shouldBe 9
+      snapshot.maxY shouldBe 9
+      snapshot.maxZ shouldBe 376
+    }
+  }
+
+  test(""" Given the example snapshot input when settled then the bricks will be settled as expected""") {
+    val snapshot = exampleSnapshotInput.toSnapshot()
+    val settled = snapshot.settle()
+
+    settled.bricks.size shouldBe snapshot.bricks.size
+
+    settled shouldBe Snapshot(
+      Brick(1, 1, 0, 1, 1, 2, 1),
+      Brick(2, 0, 0, 2, 2, 0, 2, 1),
+      Brick(3, 0, 2, 2, 2, 2, 2, 1),
+      Brick(4, 0, 0, 3, 0, 2, 3, 2, 3),
+      Brick(5, 2, 0, 3, 2, 2, 3, 2, 3),
+      Brick(6, 0, 1, 4, 2, 1, 4, 4, 5),
+      Brick(7, 1, 1, 5, 1, 1, 6, 6),
+    )
+  }
+
+  test(""" Given the custom snapshot input when settled then the bricks will be settled as expected""") {
+    val snapshot = customSnapshotInput.toSnapshot()
+    val settled = snapshot.settle()
+
+    settled.bricks.size shouldBe snapshot.bricks.size
+  }
+
+  test(""" Given the example snapshot input then the bricks that can be disintegrated can be calculated correctly""") {
+    exampleSnapshotInput.toSnapshot().settle().bricksThatCanBeDisintegrated() shouldBe setOf(2, 3, 4, 5, 7).asSequence()
+      .map { BrickId(it) }.toSet()
+  }
+
+  test(""" Given the example snapshot input then the number of bricks that can be disintegrated is 5""") {
+    exampleSnapshotInput.numberOfBricksThatCanBeDisintegrated() shouldBe 5
+  }
+
+  test(""" Given the custom snapshot input then the number of bricks that can be disintegrated is 512""") {
+    customSnapshotInput.numberOfBricksThatCanBeDisintegrated() shouldBe 512
+  }
+
+  test(""" Given the example snapshot input then the sum of other bricks to fall in chain reactions is 7""") {
+    exampleSnapshotInput.sumOfOtherBricksToFallInChainReactions() shouldBe 7
+  }
+
+  test(""" Given the custom snapshot input then the sum of other bricks to fall in chain reactions is 98167""") {
+    customSnapshotInput.sumOfOtherBricksToFallInChainReactions() shouldBe 98167
+  }
+})
